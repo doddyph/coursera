fbb25c4a730897a8dd12c73fa449239481cd3a35
Doddy PH <doddee.ph@gmail.com>
2016-06-06 11:25:24 +0700

    Week 4 - Exercise (Instructions): Using OAuth with Passport and Facebook

--- nodejs/week4/rest-server-passport/config.js
+++ nodejs/week4/rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
 	'secretKey': '12345-67890-09876-54321',
-	'mongoUrl': 'mongodb://107.102.182.149:27017/conFusion'
+	'mongoUrl': 'mongodb://107.102.182.149:27017/conFusion',
+	'facebook': {
+		clientID: 'YOUR FACEBOOK APP ID'.
+		clientSecret: 'YOUR FACEBOOK SECRET',
+		callbackURL: 'https://107.102.182.149:3443/users/facebook/callback'
+	}
 }
